5cbd1ffe292633839b8f92cd787142f059a35241
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2025-07-02 19:56:09 -0300

    Ajustes iniciais - parte 2

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import dotenv from "dotenv"
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    synchronize: true,
+    entities: ["src/model/*.ts"],
+    migrations: ["src/migrations/*.ts"]
+});

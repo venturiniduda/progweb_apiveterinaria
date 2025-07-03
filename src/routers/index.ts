175cfe49d23f37c01e17d5df1ca6e382c4c36765
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2025-07-02 21:51:02 -0300

    Ajustes Index Admin

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import PacienteRouter from "./paciente.router"
 import TutorRouter from "./tutor.router"
+import AdminRouter from "./admin.router"
 
 const router = express.Router();
 
 router.get("/paciente", PacienteRouter);
 router.get("/tutor", TutorRouter);
+router.get("/admin", AdminRouter);
 
 export default router;

206f300b7e6975218a2dca91be95ffa65e962052
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2025-07-16 19:41:17 -0300

    Complementos Swagger - Primeiros ajustes

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,26 +1,75 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
+import swaggerAutogen from 'swagger-autogen';
 
-const options: swaggerJsdoc.Options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Clínica Veterinária',
-      version: '1.0.0',
-      description: 'Documentação interativa da API da Clínica Veterinária',
-    },
-    servers: [
-      {
-        url: 'http://localhost:12345',
-      },
-    ],
+const config = {
+  info: {
+     title: 'API Clínica Veterinária',
+     version: '1.0.0',
+     description: 'Documentação interativa da API da Clínica Veterinária',
   },
-  apis: ['src/routers/*.ts'],
+  host: `localhost:${process.env.PORT || 3000}`,
+  basePath: '/api/v1',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    // Cria sessões que separa cada CRUD para cada entidade 
+    {
+      "name":"Auth",
+      "description":"Authentication endpoints"
+    },
+    {
+      "name":"Users",
+      "description":"User management endpoints"
+    },
+    {
+      "name":"Cobrança",
+      "description":"Cobranças management endpoints"
+    },
+    {
+      "name":"Consultas",
+      "description":"Consultas management endpoints"
+    },
+    {
+      "name":"Estoque",
+      "description":"Estoque management endpoints"
+    },
+    {
+      "name":"Medicamento",
+      "description":"Medicamento management endpoints"
+    },
+    // adicionar restante
+  ],
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      in: 'header',
+      name: 'Authorization',
+      description: "Insira as informações do JWT token seguindo o padrão: Bearer <token>"
+    },
+  },
+  definitions: {
+    // são os exemplos de preenchimento de cada uma das entidades
+    AddUser: {
+      $username: "testuser",
+      $password: "password123" 
+    },
+    Login: {
+      $username: "testuser",
+      $password: "password123"
+    },
+    LoginResponse: {
+      message: "Login bem sucedido!",
+      token: "dhsjgskjfgsdjhfsifuweifgsjjsldsçfdfbçcdsjcsjksçckjsdckjsç"
+    },
+    User: {
+      id: 1,
+      username: 'antonio'
+    },
+    // adicionar restante 
+  }
 };
 
-const swaggerSpec = swaggerJsdoc(options);
+const output = './docs/swagger.json';
+const endpoints = ['./src/routers/index.ts'];
 
-export function setupSwagger(app: Express): void {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-}
+swaggerAutogen()(output, endpoints, config);

251ef4a338de889d32e4661c42aaf68459e82ff8
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2025-07-15 22:34:25 -0300

    Ajustes conexão com BD

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,25 @@ import { DataSource } from "typeorm"
 import dotenv from "dotenv"
 dotenv.config();
 
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: process.env.DATABASE_HOST,
+//     port: Number(process.env.DATABASE_PORT),
+//     username: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASS,
+//     database: process.env.DATABASE_NAME,
+//     synchronize: true,
+//     entities: ["src/model/*.ts"],
+//     migrations: ["src/migrations/*.ts"]
+// });
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL, 
     synchronize: true,
     entities: ["src/model/*.ts"],
-    migrations: ["src/migrations/*.ts"]
+    migrations: ["src/migrations/*.ts"],
+    ssl: {
+        rejectUnauthorized: false
+    }
 });

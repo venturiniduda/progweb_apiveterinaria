e991e234c07e86a65f629d54263fc31da7ed1bc1
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2025-07-16 19:55:39 -0300

    Adição Documentação user.controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,22 @@
-import { Request,Response } from "express";
+import { NextFunction, Request,Response } from "express";
 import userRepostory from "../repositories/user.repository";
 
-async function addUser(req: Request, res: Response) {
+async function addUser(req: Request, res: Response, next: NextFunction) {
+    // Comentários com tags especiais que são lidas pelo swagger e interpretadas como especificação:
+    /* #swagger.tags = ['Users']
+       #swagger.description = 'Creates a new user.'
+       #swagger.parameters['body'] = {
+        in: 'body',
+        description: 'User data to create.',
+        required: true,
+        schema: { $ref: "#/definitions/AddUser" }
+       },
+       #swagger.responses[201] = {
+            description: 'User created successfully.',
+            schema: { $ref: "#/definitions/User" }
+       }
+    */
+    
     const { username, password } = req.body;
     const user = await userRepostory.addUser(username,password);
 

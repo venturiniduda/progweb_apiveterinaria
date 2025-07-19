e2b6e0976a575f2104af95094e1778e8ce81e263
Vini Xavier <vigaxa@gmail.com>
2025-07-19 00:02:45 -0300

    passwd

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,13 +1,11 @@
 import { AppDataSource } from "../data-source";
 import { Usuarios }  from "../model/user.model";
-import * as bcrypt from 'bcryptjs';
 
 async function addUser(username: string, password: string) : Promise<Usuarios> {
     const repository = AppDataSource.getRepository(Usuarios);
-    const hashedPassword = await bcrypt.hash(password, 10);
     const user = repository.create({
         "username": username,
-        "password": hashedPassword
+        "password": password
     });
     
     return await repository.save(user);
